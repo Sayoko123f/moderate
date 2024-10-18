86d5f57493cdadf460a09e527fd7bdd93f21ed69
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2024-10-18 10:27:34 +0800

    install mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 // Run `npx astro sync` after edit this file.
 export default defineConfig({
   site: "https://www.my-site.dev",
   base: "/",
   trailingSlash: "never",
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 });
