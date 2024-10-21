5af836056d0636066d9fffb4ba89954a1afb2689
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2024-10-21 13:28:09 +0800

    install type-fest

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,10 +1,12 @@
+import type { RequireExactlyOne } from "type-fest";
 import type { HTMLAttributes } from "astro/types";
 
-export interface SocialLink {
+interface _SocialLink {
   url: string;
   icon?: string;
   text?: string;
 }
+export type SocialLink = RequireExactlyOne<_SocialLink, "icon" | "text">;
 
 export interface ThemeConfig {
   lang: HTMLAttributes<"html">["lang"];
