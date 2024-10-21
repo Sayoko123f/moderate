5d822fd58ad7b319dfd4d498a359715deb7f355c
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2024-10-21 15:34:15 +0800

    update TheDesktopHeader.astro

--- src/theme-config.ts
+++ src/theme-config.ts
@@ -3,5 +3,9 @@ import type { ThemeConfig } from "@/types/theme";
 export const themeConfig: ThemeConfig = {
   lang: "en",
   siteTitle: "Welcome to Astro.",
-  socialLinks: [],
+  socialLinks: [
+    { url: "#", icon: "BiTwitterX.astro" },
+    { url: "#", icon: "MdiGithub.astro" },
+    { url: "#", text: "Any Site" },
+  ],
 };
