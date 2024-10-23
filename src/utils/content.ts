91dd7d559e398b7bb0c27a00549e5c0a49bc970b
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2024-10-23 09:22:27 +0800

    sort posts by date

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,12 +1,16 @@
 import { getCollection } from "astro:content";
-import type { CollectionEntry } from 'astro:content';
+import type { CollectionEntry } from "astro:content";
 
 export async function getPosts() {
   const postEntries = await getCollection("posts", ({ data }) => {
     return import.meta.env.PROD ? data.draft !== true : true;
   });
 
+  postEntries.sort(
+    (a, b) => b.data.published.getTime() - a.data.published.getTime(),
+  );
+
   return postEntries;
 }
 
-export type PostEntry = CollectionEntry<'posts'>;
+export type PostEntry = CollectionEntry<"posts">;
