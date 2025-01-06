84eab370ca2cdea4123d8c31e91e99538471f9ab
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2025-01-06 10:29:38 +0800

    fix: fix loadImage function

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,18 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, type SchemaContext } from "astro:content";
 import { glob } from "astro/loaders";
 
-const postSchema = z.object({
-  title: z.string(),
-  description: z.string().optional(),
-  cover: z.string().optional(),
-  category: z.string().optional(),
-  tags: z.array(z.string()),
-  draft: z.boolean().default(false),
-  published: z.date(),
-  updateAt: z.date().optional(),
-});
+const postSchema = ({ image }: SchemaContext) =>
+  z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    cover: image().optional(),
+    category: z.string().optional(),
+    tags: z.array(z.string()),
+    draft: z.boolean().default(false),
+    published: z.date(),
+    updateAt: z.date().optional(),
+  });
+
 const postCollection = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
   schema: postSchema,
--- src/utils/load-image.ts
+++ src/utils/load-image.ts
@@ -3,18 +3,35 @@ import path from "node:path";
 import { joinBaseURL } from "@/utils/url";
 
 interface LoadImageParams {
-  src: string;
-  basePath: string;
+  src: string | ImageMetadata;
+  entryFilePath?: string;
 }
 
-export async function loadImage({ src, basePath }: LoadImageParams) {
+interface LoadImageReturn {
+  isLocal: boolean;
+  isRemote: boolean;
+  localImage?: ImageMetadata;
+  urlIfNotLocal?: string;
+}
+
+export async function loadImage({
+  src,
+  entryFilePath = "/",
+}: LoadImageParams): Promise<LoadImageReturn> {
+  if (isImageMetadata(src)) {
+    return {
+      isLocal: true,
+      isRemote: false,
+      localImage: src,
+    };
+  }
   const isLocal = !(
     src.startsWith("/") ||
     src.startsWith("http") ||
     src.startsWith("https") ||
     src.startsWith("data:")
   );
-  const isPublic = src.startsWith("/");
+  const isRemote = src.startsWith("/");
   let localImage: ImageMetadata | undefined;
   if (isLocal) {
     const allImageFiles = import.meta.glob(
@@ -22,25 +39,36 @@ export async function loadImage({ src, basePath }: LoadImageParams) {
       { import: "default" },
     );
     const srcDir = "..";
-    const imagePath = path.join(srcDir, basePath, src);
+
+    const imagePath = path.join(srcDir, entryFilePath, src);
     const importPath = path.normalize(imagePath).replaceAll("\\", "/");
-    const imgModule = await allImageFiles[importPath]();
-    if (!imgModule) {
+    if (!(importPath in allImageFiles)) {
       throw new Error(
-        `[ERROR] Not found image file, originalInput=${src}, importPath=${importPath}, basePath=${basePath}.`,
+        `[ERROR] Not found image file, src=${src}, importPath=${importPath}, entryFilePath=${entryFilePath}.`,
       );
     }
+
+    const imgModule = await allImageFiles[importPath]();
     localImage = imgModule as ImageMetadata;
   }
   let urlIfNotLocal: string | undefined;
   if (!isLocal) {
-    urlIfNotLocal = isPublic ? joinBaseURL(src) : src;
+    urlIfNotLocal = isRemote ? joinBaseURL(src) : src;
   }
 
   return {
     isLocal,
-    isPublic,
+    isRemote,
     localImage,
     urlIfNotLocal,
   };
+}
+
+function isImageMetadata(obj: unknown): obj is ImageMetadata {
+  if (obj && typeof obj === "object") {
+    if ("src" in obj && "width" in obj && "height" in obj && "format" in obj) {
+      return true;
+    }
+  }
+  return false;
 }
